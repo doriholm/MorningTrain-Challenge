f916f6fc6910823f785f7183bb4d286d24abad55
doriholm <doriholm@gmail.com>
2018-04-11 09:12:49 +0200

    Minor variable adjustments

--- Backend Javascript/csvParser.js
+++ Backend Javascript/csvParser.js
@@ -1,16 +1,16 @@
 //Use file system to work with the files on the computer
-var fs = require("fs");
+const fs = require("fs");
 
 module.exports = function(csvFile) {
-  var lines = [];
+  let lines = [];
 
   // Read the CSV file
-  var f = fs.readFileSync(csvFile, { encoding: "utf-8" }, function(err) {
+  let f = fs.readFileSync(csvFile, { encoding: "utf-8" }, function(err) {
     console.log(err);
   });
 
   //Split the content into an array if there is a new line or return
-  var allTextLines = f.split(/\r\n|\n/);
+  let allTextLines = f.split(/\r\n|\n/);
 
   //Go through each element in the array and split it up where there is a comma
   //and then push it to the final array
